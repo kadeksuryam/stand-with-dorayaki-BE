31226fe9d27974c4fa50c2cf167d3de83350cdc0
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-23 04:31:28 +0700

    modif requestLogger

--- utils/middleware.js
+++ utils/middleware.js
@@ -4,6 +4,7 @@ const config = require('./config')
 const requestLogger = (req, res, next) => {
     logger.info('Method: ', req.method)
     logger.info('Path: ', req.path)
+    logger.info('Body: ', req.body)
     logger.info('---')
     next()
 }
