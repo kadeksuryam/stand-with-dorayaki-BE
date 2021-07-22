48e67edc5638a81c7db50d97292ce60ed6cdd815
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-23 04:26:51 +0700

    implement assets controller

---
+++ controllers/assets.js
@@ -0,0 +1,21 @@
+const assetsRouter = require('express').Router()
+
+assetsRouter.get('/dorayaki/:fileName', (req, res, next) => {
+    try{
+        const { fileName } = req.params
+        res.sendFile(`${fileName}`, {'root': './assets/dorayaki/'})
+    } catch(err){
+        next(err)
+    }
+})
+
+assetsRouter.get('/toko-dorayaki/:fileName', (req, res, next) => {
+    try{
+        const { fileName } = req.params
+        res.sendFile(`${fileName}`, {'root': './assets/toko-dorayaki/'})
+    } catch(err){
+        next(err)
+    }
+})
+
+module.exports = assetsRouter
