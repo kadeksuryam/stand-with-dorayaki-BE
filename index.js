3c54f2bdf4ae860764aa745115b46534d1d84106
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-27 03:02:40 +0700

    remove https

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
+const http = require('http');
 
 const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const server = https.createServer(options, app);
+const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
