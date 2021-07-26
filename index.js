c550adfe4805028d4842a44b1fa5dba5ba6ec793
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-26 22:20:22 +0700

    try serve in https

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
 const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
